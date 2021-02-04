f59b0763c7ec38ab7318bc4f28f506d50a23a443
calvert1212 <calvert1212@gmail.com>
2021-02-04 18:32:13 +0000

    Fixed betty issues 1 and 2

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -18,12 +18,14 @@ int main(void)
 	l = (n % 10);
 
 	printf("Last digit of %d is %d ", n, l);
+
 	if (l > 5)
-	    printf("and is greater than 5");
+	printf("and is greater than 5");
 	if (l == 0)
 		printf("and is 0");
 	if (l < 6 && l != 0)
 		printf("and is less than 6 and not 0");
+
 	printf("\n");
 	return (0);
 }
--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -15,5 +15,5 @@ int main(void)
 		a++;
 	}
 	putchar('\n');
-	return(0);
+	return (0);
 }

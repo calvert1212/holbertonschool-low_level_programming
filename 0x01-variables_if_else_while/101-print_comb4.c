16648c9a976e7424b7fa2a82034e3bf11ead6f65
calvert1212 <calvert1212@gmail.com>
2021-02-05 05:10:58 +0000

    Finished 100 and 101

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+ * main - prints a - z
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int b = 0;
+
+	while (b <= 9)
+	{
+		int a = b + 1;
+	while (a <= 9)
+	{
+		int c = a + 1;
+		while(c <= 9)
+		{
+		putchar((b) + 48);
+		putchar((a) + 48);
+		putchar((c) + 48);
+		if (a + b + c != 24)
+		{
+			putchar(44);
+			putchar(32);
+		}
+		c++;
+		}
+			a++;
+	}
+	b++;
+	}
+	putchar('\n');
+	return (0);
+}

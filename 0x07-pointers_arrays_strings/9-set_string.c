6cc3c7afb1add91c01f2ff97f3b336703d53107e
calvert1212 <calvert1212@gmail.com>
2021-02-23 05:24:11 +0000

    9

---
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * set_string - sets a string
+ * @s: string
+ * @to: sets string to this
+ * Return: void
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}

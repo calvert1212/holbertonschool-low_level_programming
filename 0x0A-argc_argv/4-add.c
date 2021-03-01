e2752f2e0e071d4f7b8e73ed19855d071a8cace6
calvert1212 <calvert1212@gmail.com>
2021-03-01 05:37:20 +0000

    Fixed 4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -12,8 +12,28 @@
 int main(int argc, char *argv[])
 {
 	int sum = 0;
-	int i = 1;
-	int x = 0;
+	int x, y;
 
-	if (argc >= 1 || !isdigit )
+	if (argc >= 2)
+	{
+		for (x = 1; x < argc; x ++)
+			{
+				for (y = 0; argv[x][y] != 0; y++)
+				{
+					if (!isdigit(argv[x][y]))
+					{
+						printf("Error\n");
+						return (1);
+					}
+				}
+				sum += atoi(argv[x]);
+			}
+	}
+		else
+		{
+			printf("0\n");
+			return (0);
+		}
+		printf("%d\n", sum);
+		return (0);
 }

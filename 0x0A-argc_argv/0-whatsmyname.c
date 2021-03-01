8cb7f0dcf9d9b99aad5f983fd218f7f9232b9439
calvert1212 <calvert1212@gmail.com>
2021-03-01 04:52:11 +0000

    Fixed betty error on 0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 
+/** 
+ * main - Prints name of program
+ * @argc: the argument
+ * @argv: the argument array
+ * Return: 0
+ */
+
 int main(int argc, char *argv[])
 {
 	printf("%s\n", argv[0]);

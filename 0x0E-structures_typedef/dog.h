98de4485fa884d56b867b79d6c1b9c9cfbc1c111
calvert1212 <calvert1212@gmail.com>
2021-03-08 21:59:02 +0000

    fixed 0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,7 +1,12 @@
-#include <stdio.h>
-
 #ifndef DOG_H
 #define DOG_H
+
+/**
+ * struct dog - A structure consisting of a name, age and owner name
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog's owner's name
+*/
 
 struct dog
 {

1602c3a1c4333d0c3b0a4a323d7777ce9b09aee9
calvert1212 <calvert1212@gmail.com>
2021-12-10 14:43:26 -0600

    Initial commit from github.dev vscode

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+hash_table_t nt = malloc(sizeof(hash_table_t));
+hash_node_t **arr = malloc(sizeof((hash_node_t *) * size));
+
+if (nt == NULL)
+    return (NULL);
+if (arr == NULL);
+    return (NULL);
+
+nt->size = size;
+nt->array = arr;
+return (newtable);
+}
